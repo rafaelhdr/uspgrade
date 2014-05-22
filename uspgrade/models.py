5f6a6c82a588be4e275e85111d4d7fc960c573f9
Rafael Hurpia da Rocha - Dev <rafael@hurpia.com.br>
2014-05-22 09:22:54 -0300

    Models completos (sem testes)

--- uspgrade/models.py
+++ uspgrade/models.py
@@ -4,18 +4,64 @@ from django.db import models
 
 class Usuario(models.Model):
     """ """
-	INSTITUTOS = (
-	   ('POLI', 'Escola Politécnica'),
-	   ('FEA', 'Faculdade de Economia e Administração'),
-	   ('ECA', 'Escola de Comunicação e Artes'),
-	)
-	TIPOS = (
-	   ('Visitante', 'Visitante'),
-	   ('Membro', 'Membro'),
-	   ('Responsavel', 'Responsável'),
-	   ('Moderador', 'Moderador'),
-	)
-	nome = models.CharField(max_lenght=100)
-	cpf = models.CharField(max_lenght=11)
-	instituto = models.CharField(max_lenght=50, choices=INSTITUTOS)
-	tipo = models.CharField(max_lenght=20, choices=TIPOS)
+    INSTITUTOS = (
+       ('POLI', 'Escola Politécnica'),
+       ('FEA', 'Faculdade de Economia e Administração'),
+       ('ECA', 'Escola de Comunicação e Artes'),
+    )
+    TIPOS_USUARIOS = (
+       ('Visitante', 'Visitante'),
+       ('Membro', 'Membro'),
+       ('Responsavel', 'Responsável'),
+       ('Moderador', 'Moderador'),
+    )
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11)
+    instituto = models.CharField(max_length=50, choices=INSTITUTOS)
+    tipo = models.CharField(max_length=20, choices=TIPOS_USUARIOS)
+
+class Sugestao(models.Model):
+    """"""
+    CATEGORIAS = (
+        ('acessibilidade', 'Acessibilidade'),
+        ('infraestrutura', 'Infraestrutura'),
+    )
+    INSTITUTOS = (
+       ('POLI', 'Escola Politécnica'),
+       ('FEA', 'Faculdade de Economia e Administração'),
+       ('ECA', 'Escola de Comunicação e Artes'),
+    )
+    fechada = models.BooleanField()
+    notificada = models.BooleanField()
+    data = models.DateField(auto_now_add=True)
+    categoria = models.CharField(max_length=30, choices=CATEGORIAS)
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    instituto = models.CharField(max_length=50, choices=INSTITUTOS)
+    usuario = models.ForeignKey(Usuario)
+
+class Comentario(models.Model):
+    """"""
+    data = models.DateField(auto_now_add=True)
+    conteudo = models.TextField()
+
+class Resposta(models.Model):
+    """"""
+    TIPO_RESPOSTA = (
+        ('P', 'Positiva'),
+        ('N', 'Negativa'),
+    )
+    data = models.DateField(auto_now_add=True)
+    conteudo = models.TextField()
+    tipo = models.CharField(max_length=1, choices=TIPO_RESPOSTA)
+
+class Voto(models.Model):
+    """"""
+    TIPOS_VOTO = (
+        ('D', 'Denúncia'),
+        ('E', 'Endossar'),
+        ('R', 'Rechaçar'),
+    )
+    tipo = models.CharField(max_length=1, choices=TIPOS_VOTO)
+    sugestao = models.ForeignKey(Sugestao)
+    usuario = models.ForeignKey(Usuario)
