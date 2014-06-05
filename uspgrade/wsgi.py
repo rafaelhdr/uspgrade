155feb62fdc95c4c3f23dcc577cf5b5baf4175e1
root <root@rafaelhdr.com.br>
2014-06-04 21:19:45 -0400

    wsgi edited and chmod to 775 for server

--- uspgrade/wsgi.py
+++ uspgrade/wsgi.py
@@ -7,8 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uspgrade.settings")
+
+PATH = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+sys.path.append(PATH)
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
